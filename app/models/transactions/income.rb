c95f66b9750b2ee4c1ce86681ed570c2d7a8f4f1
Mudassar Hassan <mudassarhassan135@gmail.com>
2026-04-23 17:15:51 +0500

    Remove unused models

--- app/models/transactions/income.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module Transactions
-  class Income < Transaction
-    belongs_to :category
-
-    alias_attribute :account, :destination_account
-  end
-end
--- app/models/transactions/transfer.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module Transactions
-  class Transfer < Transaction
-    def category
-      nil
-    end
-  end
-end
