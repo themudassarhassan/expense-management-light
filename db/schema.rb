5b9af3890b0f7da4218f7a5843d0bdf060e809f4
Mudassar Hassan <mudassarhassan135@gmail.com>
2026-05-07 18:24:36 +0500

    Upgrade to rails 8

--- db/schema.rb
+++ db/schema.rb
@@ -10,56 +10,56 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_05_05_200000) do
+ActiveRecord::Schema[8.1].define(version: 2026_05_07_130754) do
   create_table "accounts", force: :cascade do |t|
-    t.string "name", null: false
-    t.decimal "initial_balance", precision: 12, scale: 2, default: "0.0", null: false
-    t.boolean "system_generated", default: false, null: false
     t.string "account_type", null: false
     t.datetime "created_at", null: false
+    t.string "currency_code", default: "PKR", null: false
+    t.decimal "initial_balance", precision: 12, scale: 2, default: "0.0", null: false
+    t.string "name", null: false
+    t.boolean "system_generated", default: false, null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
-    t.string "currency_code", default: "PKR", null: false
     t.index ["user_id"], name: "index_accounts_on_user_id"
   end
 
   create_table "budgets", force: :cascade do |t|
+    t.integer "account_id", null: false
     t.decimal "amount", precision: 12, scale: 2, null: false
     t.date "budget_month", null: false
-    t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "account_id", null: false
+    t.integer "user_id", null: false
     t.index ["account_id"], name: "index_budgets_on_account_id"
     t.index ["user_id"], name: "index_budgets_on_user_id"
   end
 
   create_table "categories", force: :cascade do |t|
-    t.string "name", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.string "category_type", default: "expense", null: false
+    t.datetime "created_at", null: false
+    t.string "name", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "sessions", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.string "ip_address"
-    t.string "user_agent"
     t.datetime "created_at", null: false
+    t.string "ip_address"
     t.datetime "updated_at", null: false
+    t.string "user_agent"
+    t.integer "user_id", null: false
     t.index ["user_id"], name: "index_sessions_on_user_id"
   end
 
   create_table "transactions", force: :cascade do |t|
-    t.string "transaction_type", null: false
     t.decimal "amount", precision: 12, scale: 2, null: false
-    t.string "description"
-    t.integer "debit_account_id", null: false
-    t.integer "credit_account_id", null: false
-    t.integer "user_id", null: false
     t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.integer "credit_account_id", null: false
+    t.integer "debit_account_id", null: false
+    t.string "description"
     t.date "transaction_date", null: false
+    t.string "transaction_type", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
     t.index ["credit_account_id"], name: "index_transactions_on_credit_account_id"
     t.index ["debit_account_id"], name: "index_transactions_on_debit_account_id"
     t.index ["user_id", "transaction_date"], name: "index_transactions_on_user_id_and_transaction_date"
@@ -67,12 +67,12 @@ ActiveRecord::Schema[7.1].define(version: 2026_05_05_200000) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "base_currency", default: "PKR", null: false
+    t.datetime "created_at", null: false
     t.string "email", null: false
     t.string "name", null: false
     t.string "password_digest", null: false
-    t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "base_currency", default: "PKR", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
